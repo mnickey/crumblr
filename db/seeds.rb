cdb6dfb086bb583d6333bc47e1c8f8dbfe027d8f
Michael Nickey <mnickey@gmail.com>
2013-09-08 00:14:30 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,31 +10,29 @@
 post1 = Post.new
 post1.title = "Epic Cheese Cake"
 post.kind = "image"
-post1.content = 'http://2.bp.blogspot.com/-deAWAjfeg00/TiXub_qWspI/AAAAAAAADNA/q-mZNHTZ9Zo/s1600/cheesecake5.jpg'
+post1.content = "http://2.bp.blogspot.com/-deAWAjfeg00/TiXub_qWspI/AAAAAAAADNA/q-mZNHTZ9Zo/s1600/cheesecake5.jpg"
 post1.save
 2.times do
 	post1.hearts.create
 end
 
 # Ovenly
-post2 = Post.new(title: "Ovenly", kind: 'text')
-post2.content = 'Awesome bakery in Brooklyn. Find them here: http://oven.ly/'
+post2 = Post.new(title: "Ovenly", kind: "text")
+post2.content = "Awesome bakery in Brooklyn. Find them here: http://oven.ly/"
 post2.save
-5.times{ post2.hearts.create }
+5.times { post2.hearts.create }
 
 # Chocolate Chip Goodness
 post3 = Post.create(
-	title: 'Chocolate Chip Goodness',
-	kind: 'image',
-	content: 'http://farm4.static.flickr.com/3473/3304831464_6373d4df34.jpg'
+	title: "Chocolate Chip Goodness",
+	kind: "image",
+	content: "http://farm4.static.flickr.com/3473/3304831464_6373d4df34.jpg"
 )
 8.times { post3.hearts.create }
 
 # Tiger pouncing
-post4 = Post.create(
-	title: "Rawr",
-	kind: "image",
-	content: "http://images.nationalgeographic.com/wpf/media-live/photos/000/036/cache/diving-tiger_3629_990x742.jpg"
-)
+post4 = Post.new( title: "Rawr", kind: "image")
+post4.content = "http://images.nationalgeographic.com/wpf/media-live/photos/000/036/cache/diving-tiger_3629_990x742.jpg"
+
 post4.save
 10.times { post4.hearts.create }
